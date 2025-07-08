ebfd9540e6b86d10baa81da50df33856bc5fda9c
Paul Chan <paul.plays.a.pun@gmail.com>
2025-07-08 00:51:53 -0700

    Add 2d sgemm kernels
    Onto blackwell now

--- benchmarks/sgemm/sgemm_kernels/sgemm_2d_block_tiling.cuh
+++ benchmarks/sgemm/sgemm_kernels/sgemm_2d_block_tiling.cuh
@@ -2,11 +2,93 @@
 
 #include "../cuda_common.cuh"
 
-
-
-__global__ void sgemm_2d_block_tiling(
-
+template<const uint BM, const uint BN, const uint BK, const uint TM, const uint TN>
+__global__ void __launch_bounds((BM * BN) / (TM * TN), 1) sgemm_2d_block_tiling(
+    const int M,
+    const int N,
+    const int K,
+    float alpha,
+    float *A,
+    float *B,
+    float beta,
+    float *C
 )
 {
-    
+    const uint cRow = blockIdx.y;
+    const uint cCol = blockIdx.x;
+
+    const uint totalResultsBlocktile = BM * BN;
+    const uint numThreadsBlocktile = totalResultsBlocktile / (TM * TN);
+
+    assert(numThreadsBlocktile == blockDim.x);
+
+    const int threadCol = threadIdx.x % (BN / TN);
+    const int threadRow = threadIdx.x / (BN / TN);
+
+    __shared__ float As[BM * BK];
+    __shared__ float Bs[BK * BN];
+
+    A += cRow * BM * K;
+    B += cCol * BN;
+    C += cRow * BM * N + cCol * BN;
+
+    const uint innerRowA = threadIdx.x / BK;
+    const uint innerColA = threadIdx.x % BK;
+    const uint strideA   = numThreadsBlocktile / BK;
+    const uint innerRowB = threadIdx.x / BN;
+    const uint innerColB = threadIdx.x % BN;
+    const uint strideB   = numThreadsBlocktile / BN;
+
+    float threadResults[TM * TN] = {0.0};
+    float regM[TM] = {0.0};
+    float regN[TN] = {0.0};
+
+    for (uint bkIdx = 0; bkIdx < K; bkIdx += BK)
+    {
+        for (uint loadOffset = 0; loadOffset < BM; loadOffset += strideA)
+        {
+            As[(innerRowA + loadOffset) * BK + innerColA] =
+                A[(innerRowA + loadOffset) * K + innerColA];
+        }
+        for (uint loadOffset = 0; loadOffset < BK; loadOffset += strideB)
+        {
+            Bs[(innerRowB + loadOffset) * BN + innerColB] =
+                B[(innerRowB + loadOffset) * N + innerColB];
+        }
+        __syncthreads();
+
+        A += BK;
+        B += BK * N;
+
+        for (uint dotIdx = 0; dotIdx < BK; ++dotIdx)
+        {
+            for (uint i = 0; i < TM; ++i)
+            {
+                regM[i] = As[(threadRow * TM + i) * BK + dotIdx];
+            }
+            for (uint i = 0; i < TN; ++i)
+            {
+                regN[i] = Bs[dotIdx * BN + threadCol * TN + i];
+            }
+            for (uint resIdxM = 0; resIdxM < TM; ++resIdxM)
+            {
+                for (uint resIdxN = 0; resIdxN < TN; ++resIdxN)
+                {
+                    threadResults[resIdxM * TN + resIdxN] +=
+                        regM[resIdxM] * regN[resIdxN];
+                }
+            }
+        }
+        __syncthreads();
+    }
+
+    for (uint resIdxM = 0; resIdxM < TM; ++resIdxM)
+    {
+        for (uint resIdxN = 0; resIdxN < TN; ++resIdxN)
+        {
+            C[(threadRow * TM + resIdxM) * N + threadCol * TN + resIdxN] =
+                alpha * threadResults[resIdxM * TN + resIdxN] +
+                beta  * C[(threadRow * TM + resIdxM) * N + threadCol * TN + resIdxN];
+        }
+    }
 }
